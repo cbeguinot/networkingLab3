b9150b318064a6f3d9eb049da8309b2bfad9d880
Louis Mahl <louis.mahl@edu.ece.fr>
2018-03-26 16:44:07 +0200

    server

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -13,29 +13,47 @@ namespace FinalProject
     {
         static void Server()
         {
-            Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            sock.Listen(100);
+            int port = 5000;
 
-            Socket newSocket = sock.Accept();
+            TcpListener server = new TcpListener(IPAddress.Any, port);
 
-            IPEndPoint iep = new IPEndPoint(IPAddress.Any, 5000);
-            sock.Bind(iep);
-            EndPoint ep = (EndPoint)iep;
-            byte[] data = new byte[1024];
+            server.Start();
+
+            TcpClient client = server.AcceptTcpClient();
+
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("New user");
+
+            int nbOfData = 0;
+
+            String name = "";
+            
             for (;;)
             {
-                int recv = sock.ReceiveFrom(data, ref ep);
-                String Message = Encoding.UTF8.GetString(data, 0, recv);
-                if (Message == "quit")
+                Byte[] data = new Byte[256];
+                String responseData = String.Empty;
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+                if(nbOfData == 0)
+                {
+                    name = responseData;
+                }
+
+                if (responseData == "quit")
                 {
                     break;
                 }
 
-                Console.WriteLine("Pauline: " + Message);
-            }
+                Console.WriteLine(name + ": " + responseData);
 
-            sock.Close();
-            sock.Disconnect(true);
+                nbOfData++;
+            }
+            
+            stream.Close();
+
+            client.Close();
         }
 
         static void Main(string[] args)
@@ -48,8 +66,7 @@ namespace FinalProject
                 // You are the server
                 Console.WriteLine("You are the server");
 
-                Thread t = new Thread(Server);
-                t.Start();
+                Server();
             }
             else
             {
@@ -64,14 +81,13 @@ namespace FinalProject
                 //Socket
                 //Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp); //Ca c'est pour udp
                 Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//Ca c'est pour tcp
-                s.Connect(host, port); 
+                s.Connect(host, port);
 
                 //Coucou, toi je sais pas encore qui t'es
                 IPEndPoint iep = new IPEndPoint(IPAddress.Parse(host), port);
 
                 //C'est quoi ton p'tit nom ? Et ton adresse IP ?
                 Console.WriteLine("Ton nom : ");
-
 
                 for (;;)
                 {
